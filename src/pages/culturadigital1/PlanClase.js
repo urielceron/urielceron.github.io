cf2c96956ddb026823d898b0d10f2896e268f866
Uriel Antonio Cerón Interian <urielantonioceron@gmail.com>
2026-02-08 11:48:48 -0600

    Delete src directory

--- src/pages/culturadigital1/PlanClase.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CulturaDigitalLayout from '../CulturaDigitalLayout';
-
-const CulturaDigital1PlanClase = () => {
-  return <CulturaDigitalLayout asignatura="culturadigital1" />;
-};
-
-export default CulturaDigital1PlanClase;
